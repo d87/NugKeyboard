75952cc6a4d134f26cd722652877bdbc402eec8c
d87 <d87d87@gmail.com>
2019-10-09 17:06:25 +0700

    Coverted to Kotlin

--- app/src/main/java/com/d87/nugkeyboard/InputMethodService.kt
+++
@@ -1,7 +0,0 @@
-package com.d87.nugkeyboard
-
-import android.inputmethodservice.KeyboardView
-
-class InputMethodService {
-
-}
---
+++ app/src/main/java/com/d87/nugkeyboard/MyInputMethodService.kt
@@ -0,0 +1,95 @@
+package com.d87.nugkeyboard
+
+//import android.inputmethodservice;
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.KeyboardView
+import android.inputmethodservice.Keyboard
+import android.view.View
+import android.view.KeyEvent
+import android.text.TextUtils
+import android.view.inputmethod.InputConnection
+
+class MyInputMethodService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+
+    private var keyboardView: KeyboardView? = null
+    private var keyboard: Keyboard? = null
+
+    private var caps = false
+
+    override fun onCreateInputView(): View {
+        val kv = layoutInflater.inflate(R.layout.keyboard_view, null) as KeyboardView
+        keyboardView = kv
+        keyboard = Keyboard(this, R.xml.keys_layout)
+        kv!!.keyboard = keyboard
+        kv!!.setOnKeyboardActionListener(this)
+        return kv
+    }
+
+    override fun onPress(i: Int) {
+
+    }
+
+    override fun onRelease(i: Int) {
+
+    }
+
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray) {
+        val inputConnection = currentInputConnection
+        if (inputConnection != null) {
+            when (primaryCode) {
+                Keyboard.KEYCODE_DELETE -> {
+                    val selectedText = inputConnection.getSelectedText(0)
+
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(1, 0)
+                    } else {
+                        inputConnection.commitText("", 1)
+                    }
+                    caps = !caps
+                    keyboard!!.isShifted = caps
+                    keyboardView!!.invalidateAllKeys()
+                }
+                Keyboard.KEYCODE_SHIFT -> {
+                    caps = !caps
+                    keyboard!!.isShifted = caps
+                    keyboardView!!.invalidateAllKeys()
+                }
+                Keyboard.KEYCODE_DONE -> inputConnection.sendKeyEvent(
+                    KeyEvent(
+                        KeyEvent.ACTION_DOWN,
+                        KeyEvent.KEYCODE_ENTER
+                    )
+                )
+                else -> {
+                    var code = primaryCode.toChar()
+                    if (Character.isLetter(code) && caps) {
+                        code = Character.toUpperCase(code)
+                    }
+                    inputConnection.commitText(code.toString(), 1)
+                }
+            }
+        }
+    }
+
+
+    override fun onText(charSequence: CharSequence) {
+
+    }
+
+    override fun swipeLeft() {
+
+    }
+
+    override fun swipeRight() {
+
+    }
+
+    override fun swipeDown() {
+
+    }
+
+    override fun swipeUp() {
+
+    }
+}
